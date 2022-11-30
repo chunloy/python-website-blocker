47710ef14690e320f2848b43343a7a08a2b15fc5
Aaron Y <101907461+chunloy@users.noreply.github.com>
2022-11-29 20:01:24 -0800

    Implement function to restore hosts file to default

--- website_blocker.py
+++ website_blocker.py
@@ -9,13 +9,35 @@ import time
 # convert string to a list and remove unneeded characters
 website_list = website_list.strip("[]").replace("'", "").split(", ")
 
-
+# writes blocked websites to hosts file
 def write_to_file():
-    with open("hosts_temp", "r+") as file:
+    with open(hosts_temp, "r+") as file:
         contents = file.read()
+
+        # add all websites with redirect if not in host file
         for website in website_list:
             if not (website in contents):
                 file.write("\n{}\t{}".format(redirect, website))
+
+
+# generator expression compares each website against the current line, outputs True or False
+# outputs stored in a generator object (iterable)
+# write to file if no websites are in the current line
+def reset_file():
+    with open(hosts_temp, "r+") as file:
+        contents = file.readlines()
+
+        # move cursor to starting position
+        file.seek(0)
+
+        # check each website in list against each line in file
+        # if website isn't there, write to file
+        for line in contents:
+            if not any(website in line for website in website_list):
+                file.write(line)
+
+        # delete all text after cursor end point
+        file.truncate()
 
 
 while True:
@@ -23,12 +45,12 @@ while True:
     work_start = dt(dt.now().year, dt.now().month, dt.now().day, 8)
 
     # year, month, day, time: 5pm
-    work_end = dt(dt.now().year, dt.now().month, dt.now().day, 23)
+    work_end = dt(dt.now().year, dt.now().month, dt.now().day, 16)
 
     # during work hours, write to hosts file adding website and redirect
     if work_start < dt.now() < work_end:
         write_to_file()
     else:
-        pass
+        reset_file()
 
     time.sleep(5)  # 5 sec delay
